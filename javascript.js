faeca86d7e46f4a968fafdaf53672308c8e72d35
Chris Killen <chriskillen@gmail.com>
2023-05-23 10:45:43 +0100

    Dynamically add grid of cells and get mouseover working

--- javascript.js
+++ javascript.js
@@ -1,6 +1,22 @@
-const screen_container = document.getElementById("screen_container");
+const screen_container = document.getElementById("screen");
 const square = document.createElement('div');
 
-// add one square div to test
-square.className = 'square';
-screen_container.appendChild(square);
+let grid_size = 72;
+
+// For loop based on grid size
+for (i=0; i<grid_size; i++) {
+    // create a div
+    let cell = document.createElement("div");
+    // give it the id of square
+    cell.id = "square";
+    // change css on mouseover
+    cell.addEventListener('mouseover', function handleMouseOver() {
+        cell.id = "hovered";
+    });
+    // also change css on mouseout so it remains grey
+    cell.addEventListener('mouseout', function handleMouseOut() {
+        cell.cell.id = "hovered";
+    });
+    // add div to container
+    screen_container.appendChild(cell);
+}
