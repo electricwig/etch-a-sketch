27c2b5be859b91177d28999b5bdfb8c142af2781
Chris Killen <chriskillen@gmail.com>
2023-05-23 09:05:31 +0100

    Add one square div to test

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const screen_container = document.getElementById("screen_container");
+const square = document.createElement('div');
+
+// add one square div to test
+square.className = 'square';
+screen_container.appendChild(square);
